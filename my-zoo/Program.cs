4f1bb4c5ef20348c9a2a77dc501aca9b07cfbd3f
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-17 12:10:13 -0500

    add write mammal and write reptile method to call IIsPets and all other characteristics of the mammals and reptiles!

--- my-zoo/Program.cs
+++ my-zoo/Program.cs
@@ -11,6 +11,31 @@ namespace my_zoo
                 Console.WriteLine(animal);
                 Console.WriteLine(animal.MakeSound());
                 Console.WriteLine(animal.Eats());
+
+                if (animal is Mammal mammal)
+                {
+                    WriteMammal(mammal);
+                }
+                if (animal is Reptile reptile)
+                {
+                    WriteReptile(reptile);
+                }
+            }
+
+            static void WriteMammal(Mammal mammal)
+            {
+                if (mammal is IIsPet ispet)
+                {
+                    Console.WriteLine($"It is {ispet.IsPet} that I make for a good pet!");
+                }
+
+                Console.WriteLine($"It is {mammal.BabyIsNursed} that I nurse my babies!");
+            }
+
+            static void WriteReptile(Reptile reptile)
+            {
+                Console.WriteLine($"My scales are {reptile.ScaleColor}!");
+                Console.WriteLine($"It is {reptile.LaysEggs} that I lay eggs!");
             }
 
             Console.WriteLine("-------------------------------------------------------");
@@ -37,19 +62,20 @@ namespace my_zoo
             Alligator alligator = new Alligator();
             Console.WriteLine("Hi, I am a Alligator!");
             WriteAnimal(alligator);
-            Console.WriteLine($"Am I scary: {alligator.IsScary}");
+            Console.WriteLine($"It is {alligator.IsScary} that I am very scary!");
             Console.WriteLine();
 
             Snake snake = new Snake();
             Console.WriteLine("Hi, I am a Snake!");
             WriteAnimal(snake);
-            Console.WriteLine($"Am I scary: {snake.IsScary}");
+            Console.WriteLine($"It is {snake.IsScary} that I am very scary!");
             Console.WriteLine();
            
 
             Turtle turtle = new Turtle();
             Console.WriteLine("Hi, I am a Turtle!");
             WriteAnimal(turtle);
+            Console.WriteLine($"It is {turtle.IsPet} that I make for a good pet!");
             Console.WriteLine();
         }
     }
